2a3d781df7c795a186ac07bb2284c0c966548a46
doaa-1996 <doaaobeidat1996@gmail.com>
2021-06-13 22:41:29 +0300

    code challenge6

--- Data_Structures/linked_list/linked_list/linked_list.py
+++ Data_Structures/linked_list/linked_list/linked_list.py
@@ -5,13 +5,6 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    def insert(self, value):
-        node = Node(value)
-        if not self.head:
-            self.head = node
-        else:
-            node.next= self.head
-            self.head = node
     def append(self, value):
         node = Node(value)
         if not self.head:
@@ -33,7 +26,7 @@ class LinkedList:
                     break
                 current = current.next
             else:
-                if(current.value == value):  
+                if(current.value == value):
                     return True
             return inc
     def __str__(self):
@@ -48,5 +41,72 @@ class LinkedList:
             else:
                 out += '{ ' + str(current.value) + ' } -> None'
             return out
+    def insertBefore(self, targetValue, value):
+        # create new node
+        newNode = Node(value)
+        # find target node to append
+        node = self.head
+        if node == None:
+            print('There aren\'t any nodes to append before!')
+        else:
+            found = False
+            # search nodes
+            while node:
+                if node.next == None:
+                    break
+                if node.next.value == targetValue:
+                    found = True
+                    newNode.next = node.next
+                    node.next = newNode
+                    break
+                else:
+                    node = node.next
+            if found != True:
+                print('Your target node of {} was not found in the list!'.format(
+                    targetValue))
+
+
+
+    def insert(self, new_data):
+ 
+    # 1 & 2: Allocate the Node &
+    #        Put in the data
+     new_node = Node(new_data)
+         
+    # 3. Make next of new Node as head
+     new_node.next = self.head
+         
+    # 4. Move the head to point to new Node
+     self.head = new_node
+
+    def display(self):
+        # variable for iteration
+        x = self.head
+        # iterating until we reach the end of the linked list
+        while x != None:
+            # printing the node data
+            print(x.value, end='->')
+            # moving to the next node
+            x = x.next
+        print('Null')
+    def insertAfter(self,old_data,new_data):
+        d_new=Node(new_data)
+        temp=self.head
+        while(temp):
+            if temp.value==old_data:
+                d_new.next = temp.next
+                temp.next = d_new
+                break    
 if __name__ == "__main__":
     linklist = LinkedList()
+
+# linklist.append(1)
+# linklist.append(2)
+# linklist.append(3)
+# linklist.append(4)
+# linklist.append(5)
+# linklist.append(4)
+# linklist.insertBefore(4, 9)
+# linklist.insertAfter(1,10)
+# linklist.insert(0)
+# linklist.display()
