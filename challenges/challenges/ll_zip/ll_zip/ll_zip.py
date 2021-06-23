a1e76cbebb62a1451ab60c1169d487935369e771
doaa-1996 <doaaobeidat1996@gmail.com>
2021-06-23 22:43:23 +0300

    llziptests

--- challenges/challenges/ll_zip/ll_zip/ll_zip.py
+++ challenges/challenges/ll_zip/ll_zip/ll_zip.py
@@ -2,59 +2,57 @@ class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
- 
- 
+
+
 def printList(msg, head):
     print(msg, end='')
- 
     ptr = head
+    str=''
     while ptr:
-        print(ptr.data, end=" —> ")
+        str+=f'{ptr.data} —> '
         ptr = ptr.next
- 
-    print("None")
+    str+='None' 
+    print(str)
+    return str
 
-        
- 
- 
+
+
 def zipLists(a, b):
- 
+
     dummy = Node()
     tail = dummy
- 
+
     while True:
         if a is None:
             tail.next = b
             break
- 
+
         elif b is None:
             tail.next = a
             break
- 
+
         else:
             tail.next = a
             tail = a
             a = a.next
- 
+
             tail.next = b
             tail = b
             b = b.next
- 
+
     return dummy.next
- 
- 
+
+
 if __name__ == '__main__':
     a = b = None
-    for i in reversed(range(7, 12, 2)):
+    for i in reversed(range(1, 3, 1)):
         a = Node(i, a)
 
-    for i in reversed(range(2, 7, 2)):
+    for i in reversed(range(2, 5, 1)):
         b = Node(i, b)
 
     printList("First List: ", a)
     printList("Second List: ", b)
 
-    head = zipLists (a, b)
+    head = zipLists (a,b)
     printList("After merging: ", head)
-
-
--- challenges/challenges/ll_zip/tests/test_ll_zip.py
+++ challenges/challenges/ll_zip/tests/test_ll_zip.py
@@ -4,3 +4,48 @@ from ll_zip import __version__
 def test_version():
     assert __version__ == '0.1.0'
 
+from ll_zip.ll_zip import Node,zipLists,printList
+
+
+
+def test_llzip1():
+    a = b = None
+    for i in reversed(range(7, 12, 2)):
+        a = Node(i, a)
+
+    for i in reversed(range(2, 7, 2)):
+        b = Node(i, b) 
+
+    zippedlist = zipLists (a, b)    
+    assert printList("",zippedlist)=='7 —> 2 —> 9 —> 4 —> 11 —> 6 —> None'
+
+
+
+
+
+
+def test_llzip2():
+    a = b = None
+    for i in reversed(range(1, 6, 2)):
+        a = Node(i, a)
+
+    for i in reversed(range(2, 7, 2)):
+        b = Node(i, b) 
+
+    zippedlist = zipLists (a, b)    
+    assert printList("",zippedlist)=='1 —> 2 —> 3 —> 4 —> 5 —> 6 —> None'
+
+
+
+
+
+def test_llzip3():
+    a = b = None
+    for i in reversed(range(1, 3, 1)):
+        a = Node(i, a)
+
+    for i in reversed(range(2, 5, 1)):
+        b = Node(i, b) 
+
+    zippedlist = zipLists (a, b)    
+    assert printList("",zippedlist)=='1 —> 2 —> 2 —> 3 —> 4 —> None'
